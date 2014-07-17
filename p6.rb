377fb248c2a3563260a9ad44708cacc3cc809504
Jeff Q <jqiu1998@yahoo.com>
2014-07-17 17:58:37 -0400

    promblem 6

---
+++ p6.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+def sum_of_squares(n)
+  if n > 1
+    n*n + sum_of_squares(n-1)
+  else
+    1
+  end
+end
+
+def square_of_sums(n)
+  sum(n)*sum(n)
+end
+
+def sum(n)
+  (n*(n+1)/2).to_i
+end
+
+(100..100).each do |n|
+  a = square_of_sums(n)
+  b = sum_of_squares(n)
+  puts "#{n}: #{a} - #{b} = #{a-b}"
+end
